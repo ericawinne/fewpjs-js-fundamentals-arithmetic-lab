0cfbe25bd5260335d2c1370e96c11317f1a0656e
Erica Winne <erica.winne@powerhrg.com>
2021-05-22 17:59:27 -0400

    arithmatic-lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random())
+
+let num3 = 19
+let num4 = 5
+let mod = num3 % num4 
+
+let max = Math.max(3,4,6,20)
